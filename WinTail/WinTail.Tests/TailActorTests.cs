30b014fc4f949c02552c07a26a691af24c05ff7f
unknown <sacha.barber@gmail.com>
2015-07-03 06:40:53 +0100

    1st release

---
+++ WinTail/WinTail.Tests/TailActorTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.TestKit.NUnit;
+using FakeItEasy;
+using NUnit.Framework;
+
+namespace WinTail.Tests
+{
+    [TestFixture]
+    public class TailActorTests : TestKit
+    {
+        [Test]
+        public void Show_NotifyTailActor_When_Change_Is_Made_To_File()
+        {
+            string fullFilePath = @"c:\temp\AkkaTest.txt";
+            FileObserver fileObserver = new FileObserver(TestActor, fullFilePath);
+            fileObserver.Start();
+            File.WriteAllText(fullFilePath, "A test message from TailActorTests" +
+                DateTime.Now.ToString());
+
+            ExpectMsg<TailActor.FileWrite>(x => fullFilePath.Contains(x.FileName));
+        }
+    }
+}
